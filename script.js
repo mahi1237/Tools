6ddedc1623336ccf3379a0466e0d38de71d18145
mahi1237 <shrigadimahendra9@gmail.com>
2025-06-07 20:01:36 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function showDate() {
+    alert("Today's date is: " + new Date().toDateString());
+}
+
+function randomNumber() {
+    alert("Random number: " + Math.floor(Math.random() * 100));
+}
+
+function greet() {
+    alert("Hello, welcome to my Tools site!");
+}
